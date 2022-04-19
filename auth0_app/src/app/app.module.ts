80ab58b8a264c33f9b7c32f01b72883b46b88c3f
Alfonso Valenciana <valencianaad@hotmail.com>
2022-04-18 22:40:31 -0500

    Upload simple test project using Auth0

--- auth0_app/src/app/app.module.ts
+++ auth0_app/src/app/app.module.ts
@@ -6,19 +6,29 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProtectedComponent } from './components/protected/protected.component';
+import { PricesComponent } from './components/prices/prices.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    ProtectedComponent
+    ProtectedComponent,
+    PricesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Import the module into the application, with configuration
+    AuthModule.forRoot({
+      domain: 'dev-y3ka8l0s.us.auth0.com',
+      clientId: 'pSqqkzz5AU0dEeTMLU7nBgns0v2Zntdr',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
