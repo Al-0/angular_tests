073106db4d4fb0698aeb448d46079fbb67f930ca
Alfonso Valenciana <valencianaad@hotmail.com>
2022-06-23 17:42:19 -0500

    Install ngboostrap 13 beta

--- moviesApp/src/app/app.module.ts
+++ moviesApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +15,8 @@ import { PagesModule } from './pages/pages.module';
     AppRoutingModule,
     HttpClientModule,
     ComponentsModule,
-    PagesModule
+    PagesModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent],
