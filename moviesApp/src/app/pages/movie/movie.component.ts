a58d09b41f35175e98137ddb9811150af97247ff
Alfonso Valenciana <valencianaad@hotmail.com>
2022-07-02 23:42:45 -0500

    Use combineLatest

--- moviesApp/src/app/pages/movie/movie.component.ts
+++ moviesApp/src/app/pages/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 import { Cast } from 'src/app/interfaces/credits';
 import { MovieDetails } from 'src/app/interfaces/movie-details';
 import { MoviesService } from 'src/app/services/movies.service';
@@ -24,17 +25,30 @@ export class MovieComponent implements OnInit {
   ngOnInit(): void {
     const { id } = this.activatedRoute.snapshot.params;
 
-    this.moviesService.getMovieDetails(id).subscribe((res) => {
-      if (!res){
+    combineLatest([
+      this.moviesService.getMovieDetails(id),
+      this.moviesService.getCast(id),
+    ]).subscribe(([movie, cast]) => {
+      if (!movie){
         this.router.navigateByUrl('/home');
         return;
       }
-      this.movie = res;
+      this.movie = movie;
+
+      this.cast = cast.filter(actor => actor.profile_path);
     });
 
-    this.moviesService.getCast(id).subscribe((res) => {
-      this.cast = res.filter(actor => actor.profile_path);
-    });
+    // this.moviesService.getMovieDetails(id).subscribe((res) => {
+      // if (!res){
+      //   this.router.navigateByUrl('/home');
+      //   return;
+      // }
+      // this.movie = res;
+    // });
+
+    // this.moviesService.getCast(id).subscribe((res) => {
+      // this.cast = res.filter(actor => actor.profile_path);
+    // });
   }
 
   onGoBack() {
