fa63df71881136783ece3171cc55b11b938e2835
Alfonso Valenciana <valencianaad@hotmail.com>
2022-07-24 23:39:17 -0500

    Start graphics and backend

--- firebaseOverview/src/app/components/components.module.ts
+++ firebaseOverview/src/app/components/components.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HorizontalLineChartComponent } from './horizontal-line-chart/horizontal-line-chart.component';
 
@@ -12,7 +14,9 @@ import { HorizontalLineChartComponent } from './horizontal-line-chart/horizontal
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgxChartsModule,
+    BrowserAnimationsModule
   ],
   exports: [
     NavbarComponent,
--- firebaseOverview/src/app/components/horizontal-line-chart/horizontal-line-chart.component.ts
+++ firebaseOverview/src/app/components/horizontal-line-chart/horizontal-line-chart.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-horizontal-line-chart',
   templateUrl: './horizontal-line-chart.component.html',
-  styleUrls: ['./horizontal-line-chart.component.css']
+  styleUrls: ['./horizontal-line-chart.component.css'],
 })
-export class HorizontalLineChartComponent implements OnInit {
+export class HorizontalLineChartComponent implements OnDestroy {
+  data = [
+    {
+      "name": "Game 1",
+      "value": 15
+    },
+    {
+      "name": "Game 2",
+      "value": 5
+    },
+    {
+      "name": "Game 3",
+      "value": 20
+    },
+    {
+      "name": "Game 4",
+      "value": 12
+    }
+  ];;
 
-  constructor() { }
+  // options
+  showXAxis: boolean = true;
+  showYAxis: boolean = true;
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showXAxisLabel: boolean = true;
+  yAxisLabel: string = 'Games';
+  showYAxisLabel: boolean = true;
+  xAxisLabel: string = 'Votes';
 
-  ngOnInit(): void {
+  colorScheme = 'nightLights';
+
+  interval;
+
+  constructor() {
+    this.interval = setInterval(() => {
+      const newValues = [...this.data];
+
+      for (const item of newValues){
+        item.value = Math.round( Math.random() * 500 );
+      }
+
+      this.data = [...newValues];
+    }, 1500)
   }
 
+  onSelect(data: any): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
